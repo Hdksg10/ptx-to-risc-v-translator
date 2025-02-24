26d365520bec7373ca0d6c530b3d415873af5dce
WuMianzhi <1427207878@qq.com>
2025-02-25 00:34:27 +0800

    add clean cricket build dir script

--- tools/build.sh
+++ tools/build.sh
@@ -7,6 +7,8 @@ BUILD_DIR="$PARENT_DIR/build"
 if [ "$1" == "clean" ]; then
     echo "Cleaning build directory: $BUILD_DIR"
     rm -rf "$BUILD_DIR"
+    echo "Cleaning cricket build directory..."
+    (cd $PARENT_DIR/deps/3rd/cricket && make clean)
     echo "Build directory removed."
     exit 0
 fi
